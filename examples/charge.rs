025d8325b71fe3dc7abacaefe2b47f5df066b496
Edouard Paris <m@edouard.paris>
2019-07-22 13:17:03 +0200

    examples charge get ID

--- examples/charge.rs
+++ examples/charge.rs
@@ -1,12 +1,16 @@
 use futures::future::{lazy};
 use actix_rt::System;
-use clap::{Arg, App, SubCommand};
+use clap::{Arg, ArgMatches, App, SubCommand};
 
 use opennode::client::Client;
 use opennode::charge;
 
-
-/// cargo run --example charge -- --key=<KEY> create --amount=2000
+/// Create a new charge:
+/// `cargo run --example charge -- --key=<KEY> create --amount=2000`
+///
+/// Get a charge with the given id:
+/// `cargo run --example charge -- --key=<KEY> get <ID>`
+///
 fn main() {
     let app = App::new("charge")
         .arg(Arg::with_name("key")
@@ -23,18 +27,40 @@ fn main() {
                          .value_name("AMOUNT")
                          .help("charge amount in satoshis")
                          .required(true)
+                         .takes_value(true)))
+       .subcommand(SubCommand::with_name("get")
+                    .about("retrieve a charge with the given id")
+                    .arg(Arg::with_name("id")
+                         .value_name("ID")
+                         .help("id of the charge")
+                         .required(true)
                          .takes_value(true)));
 
     let matches = app.get_matches();
     let api_key = matches.value_of("key").unwrap();
-
-
     let client = Client::from_url("https://dev-api.opennode.co", api_key);
 
-    let mut test = System::new("test");
+    match matches.subcommand() {
+        ("create", Some(m)) => create(m, &client),
+        ("get", Some(m)) => get(m, &client),
+        _ => (),
+    }
+}
 
-    let charge: charge::Charge = test.block_on(lazy(|| {
-        charge::create(&client, charge::Payload::new(2000))
+fn create(matches: &ArgMatches, client: &Client) {
+    let a = matches.value_of("amount").unwrap();
+    let amount = a.parse::<u64>().unwrap();
+    let charge: charge::Charge = System::new("test").block_on(lazy(|| {
+        charge::create(&client, charge::Payload::new(amount))
+    })).unwrap();
+
+    println!("{:?}", charge)
+}
+
+fn get(matches: &ArgMatches, client: &Client) {
+    let id = matches.value_of("id").unwrap();
+    let charge: charge::Charge = System::new("test").block_on(lazy(|| {
+        charge::get(&client, id)
     })).unwrap();
 
     println!("{:?}", charge)
